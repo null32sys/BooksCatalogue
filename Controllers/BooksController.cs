9240c639fdc997b53bba0a52619b614959a888ec
null32sys <coldcloud87@gmail.com>
2020-02-12 13:36:23 +0700

    Update BooksController.cs

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -15,12 +15,12 @@ namespace BooksCatalogue.Controllers
     {
         // private string apiEndpoint = "https://bookscatalogueapi-dicoding.azurewebsites.net/api/books/";
         private string apiEndpoint = "https://localhost:8000/api/books/";
-        private readonly HttpClient _client;
-        public BooksController()
+        
+        HttpClientHandler clientHandler = new HttpClientHandler(clientHandler);
+        public BooksController(clientHandler)
         {
             // Use this client handler to bypass ssl policy errors
-            // clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            _client = new HttpClient();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
         }
 
         // GET: Books
